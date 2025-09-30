e351f0acaff7583bd6c9721f7e62df85a6d32240
c0a2425173 <c0a2425173@edu.teu.ac.jp>
2025-09-30 16:31:38 +0900

    演習課題 追加機能２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -43,6 +43,17 @@ def gameover(screen: pg.Surface) -> None:
     time.sleep(5)
 
 
+def init_bb_imgs() -> tuple[list[pg.Surface],list[int]]:
+    bb_imgs = []
+    for r in range(1,11):
+        bb_img = pg.Surface((20*r,20*r))
+        pg.draw.circle(bb_img,(255,0,0),(10*r,10*r),10*r)
+        bb_img.set_colorkey((0,0,0))
+        bb_imgs.append(bb_img)
+    bb_accs = [a for a in range(1, 11)]
+    return bb_imgs, bb_accs
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -59,6 +70,10 @@ def main():
     vx,vy = +5,+5 #爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
+
+    bb_imgs,bb_accs =init_bb_imgs()   
+    
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -67,6 +82,10 @@ def main():
         if kk_rct.colliderect(bb_rct): #効果とんの爆弾の衝突判定
             gameover(screen)
             return #ゲームオーバー
+        
+        avx = vx*bb_accs[min(tmr//500,9)]
+        avy = vy*bb_accs[min(tmr//500,9)]
+        bb_img = bb_imgs[min(tmr//500,9)]
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -85,7 +104,7 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
-        bb_rct.move_ip(vx,vy)
+        bb_rct.move_ip(avx,avy)
         yoko,tate = check_bound(bb_rct)
         if not yoko: #横方向にはみ出ていたら 
             vx *= -1
